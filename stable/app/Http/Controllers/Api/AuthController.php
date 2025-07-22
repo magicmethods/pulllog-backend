0eaa3a13111493089d450583230d47df77e54e72
Katsuhiko Maeno <katsuhiko.maeno@crossrink.co.jp>
2025-07-23 00:14:24 +0900

    fix typo of AuthController class

--- stable/app/Http/Controllers/Api/AuthController.php
+++ stable/app/Http/Controllers/Api/AuthController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Api\DefaultApiInterface;
 //use App\Http\Controllers\Controller;
 
-class DefaultController extends Controller
+class AuthController extends Controller
 {
     /**
      * Constructor
